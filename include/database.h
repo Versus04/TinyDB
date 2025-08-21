cb8e52bb868f3c7d56d06ce2e4e275cd4d7bc0eb
Shubham Kumar Bind <115348960+Versus04@users.noreply.github.com>
2025-08-21 18:22:29 +0530

    Implement core database operations: CREATE, INSERT, SELECT, and EXIT - Added functionality to handle CREATE commands, including schema validation. - Implemented INSERT command to add rows to tables with column count checks. - Enhanced SELECT command to support column filtering and wildcard (*) queries. - Added EXIT command to gracefully terminate the database.

--- include/database.h
+++ include/database.h
@@ -1,6 +1,14 @@
 #include <iostream>
-
+#include <vector>
+#include <unordered_map>
+struct Table{
+    std::vector<std::string> coltype;
+    std::vector<std::string>colname;
+    std::vector<std::vector<std::string>> rows;
+};
 class database{
+    private:
+    std::unordered_map<std::string,Table> tables;
     public:
-    void execute(std:: string& query);
+    void execute(const command& cmd);
 };
--- src/database.cpp
+++ src/database.cpp
@@ -1,6 +1,85 @@
 #include <iostream>
+#include "command.h"
 #include "database.h"
-void database::execute( std::string& query)
+#include <unordered_map>
+#include <algorithm>
+void database::execute( const command& cmd)
 {
-    std::cout<<"Executing"<<query<<std::endl;
+    if(cmd.type==commandType::CREATE)
+    {
+        if(tables.find(cmd.tablename)!=tables.end())
+        {
+            std::cout<<"Error: Table"<<cmd.tablename<<" already exists.\n";
+            return ;
+        }
+        Table t;
+        t.colname=cmd.columns;
+        t.coltype=cmd.columnTypes;
+        tables[cmd.tablename]=t;
+        std::cout<<"Table "<<cmd.tablename<<" created \n";
+    }
+    else if(cmd.type==commandType::INSERT){
+        if(tables.find(cmd.tablename)==tables.end())
+        {
+            std::cout<<"Error Table "<<cmd.tablename<<" doesn't exist\n";
+            return ;
+        }
+        Table& t = tables[cmd.tablename];
+        if(cmd.values.size()!=t.colname.size()){
+            std::cout << "Error: Column count mismatch.\n";
+            return;
+        }   
+        t.rows.push_back(cmd.values);
+        std::cout << "Inserted 1 row into " << cmd.tablename << ".\n";
+    }
+    else if(cmd.type==commandType::SELECT){
+        if(tables.find(cmd.tablename)==tables.end()){
+            std::cout << "Error: Table " << cmd.tablename << " does not exist.\n";
+            return;
+        }
+        Table& t = tables[cmd.tablename];
+        if(cmd.columns.size()==1 && cmd.columns[0]=="*"){
+            for (auto& col : t.colname) {
+                std::cout << col << "\t";
+            }
+             std::cout << "\n";
+             for(auto& row : t.rows){
+                for(auto& val : row){
+                    std::cout<<val<<"\t";
+                }
+                std::cout<<"\n";
+             }
+        }
+        else{
+            std::vector<int>indices;
+            for(auto& colName  : cmd.columns){
+                auto it = std::find(t.colname.begin(), t.colname.end(), colName);
+                if(it!=t.colname.end()){
+                    indices.push_back(it-t.colname.begin());
+                }else{
+                    std::cout << "Error: Column " << colName << " not found.\n";
+                    return;
+                }
+            }
+            for(auto& colname : cmd.columns){
+            std::cout<<colname<<"\t";
+                }
+                std::cout<<"\n";
+                for(auto& rows: t.rows ){
+                    for(int idx: indices){
+                        std::cout<<rows[idx]<<"\t";
+                    }
+                    std::cout<<"\n";
+                }
+        }
+       
+
+        } else if(cmd.type==commandType::EXIT){
+            std::cout << "Exiting database...\n";
+        exit(0);
+        
+    }
+     else {
+        std::cout << "Unknown command.\n";
+    }
 }
